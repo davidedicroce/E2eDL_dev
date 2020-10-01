d3d0a93f1544dc41032519eb0e52595d734d154d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-02 03:25:26 +0530

    Update JetFrameProducer.cc

--- FrameProducers/plugins/JetFrameProducer.cc
+++ FrameProducers/plugins/JetFrameProducer.cc
@@ -107,8 +107,202 @@ JetFrameProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    }
    edm::Handle<std::vector<float>> HBHEenergy_handle;
    iEvent.getByToken(HBHEenergy_token, HBHEenergy_handle);
-  // Load required tokens into input collection handles
+   // Load required tokens into input collection handles
   
+   return;
+}
+
+// ------------ method called once each stream before processing any runs, lumis or events  ------------
+void
+JetFrameProducer::beginStream(edm::StreamID)
+{
+ nTotal = 0;
+ nPassed = 0;
+ std::cout<<"'TopProducer' Stream began"<<std::endl;
+}
+
+// ------------ method called once each stream after processing all runs, lumis and events  ------------
+void
+JetFrameProducer::endStream() {
+ std::cout << "'TopProducer' selected: " << nPassed << "/" << nTotal << std::endl;
+}
+
+// ------------ method called when starting to processes a run  ------------
+/*
+void
+JetFrameProducer::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method called when ending the processing of a run  ------------
+/*
+void
+JetFrameProducer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void
+JetFrameProducer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void
+JetFrameProducer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+JetFrameProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+const reco::PFCandidate*
+JetFrameProducer::getPFCand(edm::Handle<PFCollection> pfCands, float eta, float phi, float& minDr, bool debug ) {
+
+  minDr = 10;
+  const reco::PFCandidate* minDRCand = nullptr;
+  
+  for ( PFCollection::const_iterator iPFC = pfCands->begin();
+        iPFC != pfCands->end(); ++iPFC ) {
+
+    const reco::Track* thisTrk = iPFC->bestTrack();
+    if ( !thisTrk ) continue;
+
+    float thisdR = reco::deltaR( eta, phi, thisTrk->eta(), thisTrk->phi() );
+    if (debug) std::cout << "\tthisdR: " << thisdR << " " << thisTrk->pt() << " " << iPFC->particleId() << std::endl;
+
+    const reco::PFCandidate& thisPFCand = (*iPFC);
+      
+    if ( (thisdR < 0.01) && (thisdR <minDr) ) {
+      minDr    = thisdR; 
+      minDRCand = &thisPFCand;
+    }
+  }
+
+  return minDRCand;  
+}
+
+const reco::Track*
+JetFrameProducer::getTrackCand(edm::Handle<reco::TrackCollection> trackCands, float eta, float phi, float& minDr, bool debug ) {
+
+  minDr = 10;
+  const reco::Track* minDRCand = nullptr;
+  reco::Track::TrackQuality tkQt_ = reco::Track::qualityByName("highPurity");
+
+  for ( reco::TrackCollection::const_iterator iTk = trackCands->begin();
+        iTk != trackCands->end(); ++iTk ) {
+    if ( !(iTk->quality(tkQt_)) ) continue;  
+
+    float thisdR = reco::deltaR( eta, phi, iTk->eta(),iTk->phi() );
+    if (debug) std::cout << "\tthisdR: " << thisdR << " " << iTk->pt() << std::endl;
+
+    const reco::Track& thisTrackCand = (*iTk);
+      
+    if ( (thisdR < 0.01) && (thisdR <minDr) ) {
+      minDr    = thisdR; 
+      minDRCand = &thisTrackCand;
+    }
+  }
+
+  return minDRCand;  
+}
+
+
+
+
+int JetFrameProducer::getTruthLabel(const reco::PFJetRef& recJet, edm::Handle<reco::GenParticleCollection> genParticles, float dRMatch , bool debug ){
+  if ( debug ) {
+    std::cout << " Mathcing reco jetPt:" << recJet->pt() << " jetEta:" << recJet->eta() << " jetPhi:" << recJet->phi() << std::endl;
+  }
+
+  for (reco::GenParticleCollection::const_iterator iGen = genParticles->begin();
+       iGen != genParticles->end();
+       ++iGen) {
+
+    // From: (page 7/ Table 1.5.2)
+    //https://indico.desy.de/indico/event/7142/session/9/contribution/31/material/slides/6.pdf
+    //code range explanation:
+    // 11 - 19 beam particles
+    // 21 - 29 particles of the hardest subprocess
+    // 31 - 39 particles of subsequent subprocesses in multiparton interactions
+    // 41 - 49 particles produced by initial-state-showers
+    // 51 - 59 particles produced by final-state-showers
+    // 61 - 69 particles produced by beam-remnant treatment
+    // 71 - 79 partons in preparation of hadronization process
+    // 81 - 89 primary hadrons produced by hadronization process
+    // 91 - 99 particles produced in decay process, or by Bose-Einstein effects
+
+    // Do not want to match to the final particles in the shower
+    if ( iGen->status() > 99 ) continue;
+    
+    // Only want to match to partons/leptons/bosons
+    if ( iGen->pdgId() > 25 ) continue;
+
+    float dR = reco::deltaR( recJet->eta(),recJet->phi(), iGen->eta(),iGen->phi() );
+
+    if ( debug ) std::cout << " \t >> dR " << dR << " id:" << iGen->pdgId() << " status:" << iGen->status() << " nDaught:" << iGen->numberOfDaughters() << " pt:"<< iGen->pt() << " eta:" <<iGen->eta() << " phi:" <<iGen->phi() << " nMoms:" <<iGen->numberOfMothers()<< std::endl;
+
+    if ( dR > dRMatch ) continue; 
+    if ( debug ) std::cout << " Matched pdgID " << iGen->pdgId() << std::endl;
+
+    return iGen->pdgId();
+
+  } // gen particles 
+
+
+
+
+
+  return -99;
+}
+
+
+float JetFrameProducer::getBTaggingValue(const reco::PFJetRef& recJet, edm::Handle<edm::View<reco::Jet> >& recoJetCollection, edm::Handle<reco::JetTagCollection>& btagCollection, float dRMatch, bool debug ){
+
+  // loop over jets
+  for( edm::View<reco::Jet>::const_iterator jetToMatch = recoJetCollection->begin(); jetToMatch != recoJetCollection->end(); ++jetToMatch )
+    {
+      reco::Jet thisJet = *jetToMatch;
+      float dR = reco::deltaR( recJet->eta(),recJet->phi(), thisJet.eta(),thisJet.phi() );
+      if(dR > 0.1) continue;
+
+      size_t idx = (jetToMatch - recoJetCollection->begin());
+      edm::RefToBase<reco::Jet> jetRef = recoJetCollection->refAt(idx);
+
+      if(debug) std::cout << "btag discriminator value = " << (*btagCollection)[jetRef] << std::endl;
+      return (*btagCollection)[jetRef];
+  
+    }
+
+  if(debug){
+    std::cout << "ERROR  No btag match: " << std::endl;
+    
+    // loop over jets
+    for( edm::View<reco::Jet>::const_iterator jetToMatch = recoJetCollection->begin(); jetToMatch != recoJetCollection->end(); ++jetToMatch )
+      {
+	const reco::Jet thisJet = *jetToMatch;
+	std::cout << "\t Match attempt pt: " <<  thisJet.pt() << " vs " <<  recJet->pt()
+		  << " eta: " << thisJet.eta() << " vs " << recJet->eta()
+		  << "phi: "<< thisJet.phi() << " vs " << recJet->phi()
+		  << std::endl;
+	float dR = reco::deltaR( recJet->eta(),recJet->phi(), thisJet.eta(),thisJet.phi() );
+	std::cout << "dR " << dR << std::endl;
+      }
+  }    
+
+  return -99;
 }
 
 //define this as a plug-in
