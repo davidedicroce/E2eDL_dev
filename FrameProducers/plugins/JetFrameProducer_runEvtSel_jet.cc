6119341579259a4fd70fc77c346cad37f433d858
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-02 04:51:42 +0530

    Update JetFrameProducer_runEvtSel_jet.cc

--- FrameProducers/plugins/JetFrameProducer_runEvtSel_jet.cc
+++ FrameProducers/plugins/JetFrameProducer_runEvtSel_jet.cc
@@ -24,7 +24,7 @@ bool JetFrameProducer::runEvtSel_jet ( const edm::Event& iEvent, const edm::Even
    float seedE;
    int iphi_, ieta_, ietaAbs_;
    int nJet = 0;
-   e2e::Frame2D    vJetSeeds ( jets->size(), e2e::seed(nSeedCoords, int(defaultVal)) );
+   e2e::Frame2D    vJetSeeds ( jets->size(), std::vector<float> (nSeedCoords, float(defaultVal)) );
    vFailedJetIdx_.clear();
    
    std::cout<<" >> Reading and selecting Jets from "<<jets->size()<<" jet seeds: "<<std::endl;
