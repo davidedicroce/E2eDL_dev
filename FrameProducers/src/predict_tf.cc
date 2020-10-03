17a64102c5015eb2b9795d2070fc4db91098d6d6
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-04 02:28:26 +0530

    Update predict_tf.cc

--- FrameProducers/src/predict_tf.cc
+++ FrameProducers/src/predict_tf.cc
@@ -26,7 +26,7 @@ e2e::Frame2D e2e::predict_tf(e2e::Frame4D& vinputFrame, string model_filename, s
   }
  }*/
   
- tensorflow::Tensor x(tensorflow::DT_FLOAT, tensorflow::TensorShape({batch_sz, frame_height, frame_width, no_channels}));
+ tensorflow::Tensor x(tensorflow::DT_FLOAT, tensorflow::TensorShape({batch_sz, no_channels, frame_height, frame_width}));
  auto _XTensor = x.tensor<float,4>();
  for (int batch_idx=0; batch_idx<batch_sz; batch_idx++){
   for (int row_idx=0;row_idx<frame_height; row_idx++){
