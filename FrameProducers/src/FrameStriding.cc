ba963560593143c27a49172c76972265c1636e80
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-05 03:44:56 +0530

    Update FrameStriding.cc

--- FrameProducers/src/FrameStriding.cc
+++ FrameProducers/src/FrameStriding.cc
@@ -7,8 +7,8 @@ e2e::Frame1D frameStriding(std::vector<float>& vDetFrame, int rows, int columns,
     for (int colidx=0; colidx<columns; colidx++){
       for (int kernelrow=0; kernelrow<rowstrides; kernelrow++){
         for (int kernelcol=0; kernelcol<colstrides; kernelcol++){
-          vStridedFrame[(rowstrides*rowidx+kernelrow)*columns+colstrides*colidx+kernelcol] = vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
-          //if(rowidx<5 && colidx<5) std::cout<<"("<<rowstrides*rowidx+kernelrow<<","<<colstrides*colidx+kernelcol<<"): "<<vStridedFrame[rowstrides*rowidx+kernelrow][colstrides*colidx+kernelcol]<<" "<<vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
+          vStridedFrame[(rowstrides*rowidx+kernelrow)*columns*colstrides+colstrides*colidx+kernelcol] = vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
+          //if(rowidx<5 && colidx<5) std::cout<<"("<<rowstrides*rowidx+kernelrow<<","<<colstrides*colidx+kernelcol<<"): "<<vStridedFrame[(rowstrides*rowidx+kernelrow)*columns*colstrides+colstrides*colidx+kernelcol]<<" "<<vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
         }
       }
     }
