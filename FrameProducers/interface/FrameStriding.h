02803f9cbfa2148e08fa40198fd448dfcd16e479
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-05 02:35:16 +0530

    Create FrameStriding.h

---
+++ FrameProducers/interface/FrameStriding.h
@@ -0,0 +1,11 @@
+#ifndef E2eReco_frameStriding_h
+#define E2eReco_frameStriding_h
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "E2eDL/DataFormats/interface/FrameCollections.h"
+
+e2e::Frame1D frameStriding(e2e::Frame1D&, int, int, int, int);
+
+#endif
