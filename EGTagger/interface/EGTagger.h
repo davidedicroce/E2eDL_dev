6c8416d821545ca3ed4e95bd33444b4de2a5df61
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-06 01:30:43 +0530

    Update EGTagger.h

--- EGTagger/interface/EGTagger.h
+++ EGTagger/interface/EGTagger.h
@@ -50,8 +50,8 @@ class EGTagger : public edm::stream::EDProducer<> {
       // ----------member data ---------------------------
       // Input tokens
       edm::EDGetTokenT<PhotonCollection>           tPhotonCollection;
-      edm::EDGetTokenT<std::vector<e2e::Frame3D> > tEGframeCollection;
-
+      //edm::EDGetTokenT<std::vector<e2e::Frame3D> > tEGframeCollection;
+      edm::EDGetTokenT<e2e::PhoFrame3DCollection> tEGframeCollection;
       // Handles
       edm::Handle<PhotonCollection>           hPhoton;
       edm::Handle<std::vector<e2e::Frame3D> > hEGframe;
