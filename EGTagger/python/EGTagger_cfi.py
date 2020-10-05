e3b3b2980528478f047a99fbf58197e4d6011763
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-06 04:36:56 +0530

    Update EGTagger_cfi.py

--- EGTagger/python/EGTagger_cfi.py
+++ EGTagger/python/EGTagger_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 EGTagger = cms.EDProducer('EGTagger'
     , photonCollection = cms.InputTag('gedPhotons')
-    , EGFrames = cms.InputTag('EGFrameProducer','EGFrames')
+    , EGFrames = cms.InputTag('EGFrames','EGFrames')
     , EGModelName = cms.string('e_vs_ph_model.pb')
     )
