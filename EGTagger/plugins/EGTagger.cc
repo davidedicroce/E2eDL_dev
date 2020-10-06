d25e0f5f4241294368fc7ad7930b44e7e792cde4
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-10-06 18:38:24 +0530

    Update EGTagger.cc

--- EGTagger/plugins/EGTagger.cc
+++ EGTagger/plugins/EGTagger.cc
@@ -28,53 +28,55 @@ EGTagger::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByToken( tPhotonCollection,  hPhoton  );
   iEvent.getByToken( tEGframeCollection, hEGframe );
   assert( hPhoton->size() == hEGframe->size() );
-
-  // Get pointer to input EG frames
-  const std::vector<e2e::Frame3D>* pEGframe = hEGframe.product();
-  nFrameD = pEGframe->front().size(); // get size of depth dimension
-
-  // Initialize product values to be stored with default values at start of every event
-  // Each object is a vector over the no. of photons in the event
+  
   nPhos = hPhoton->size();
   std::vector<e2e::pred>    vPhoProbs ( nPhos, defaultVal );
-  std::vector<e2e::Frame3D> vPhoFrames( nPhos,
+  if (hEGframe->size>0) {
+    // Get pointer to input EG frames
+    const std::vector<e2e::Frame3D>* pEGframe = hEGframe.product();
+    nFrameD = pEGframe->front().size(); // get size of depth dimension
+
+    // Initialize product values to be stored with default values at start of every event
+    // Each object is a vector over the no. of photons in the event
+  
+    std::vector<e2e::Frame3D> vPhoFrames( nPhos,
                                         e2e::Frame3D(nFrameD,
                                         e2e::Frame2D(nFrameH,
                                         e2e::Frame1D(nFrameW, 0.))) );
 
-  //_____ Load EG frame collection into `vPhoFrames` for each photon _____//
+    //_____ Load EG frame collection into `vPhoFrames` for each photon _____//
 
-  for ( unsigned int iP = 0; iP < hPhoton->size(); iP++ ) {
-    // Get EG frame for this photon
-    PhotonRef iRecoPho( hPhoton, iP );
-    vPhoFrames[iP] = pEGframe->at(iP);
-  } // photons
+    for ( unsigned int iP = 0; iP < hPhoton->size(); iP++ ) {
+      // Get EG frame for this photon
+      PhotonRef iRecoPho( hPhoton, iP );
+      vPhoFrames[iP] = pEGframe->at(iP);
+    } // photons
 
-  //_____ Run DL inference _____//
+    //_____ Run DL inference _____//
 
-  // Run inference on `vPhoFrames` batch of size nPhos*nFrameD*nFrameH*nFrameW: store output in `vPhoProbs`
-  // Running on entire batch at once maximizes computing parellization
-  // runInference( vPhoProbs, vPhoFrames, modelName );
+    // Run inference on `vPhoFrames` batch of size nPhos*nFrameD*nFrameH*nFrameW: store output in `vPhoProbs`
+    // Running on entire batch at once maximizes computing parellization
+    // runInference( vPhoProbs, vPhoFrames, modelName );
   
-  e2e::Frame2D tmp_out = e2e::predict_tf(vPhoFrames, "e_vs_ph_model.pb", "inputs","softmax_1/Sigmoid");
+    e2e::Frame2D tmp_out = e2e::predict_tf(vPhoFrames, "e_vs_ph_model.pb", "inputs","softmax_1/Sigmoid");
   
-  //_____ Store products associated with each photon _____//
+    //_____ Store products associated with each photon _____//
 
-  // Initialize pointers to edm::AssociationVector (key,val) collections
-  // These collections create explicit associations between the photon object (key) and the stored product (val)
+    // Initialize pointers to edm::AssociationVector (key,val) collections
+    // These collections create explicit associations between the photon object (key) and the stored product (val)
+  }
   cPhoProbs  = std::make_unique<e2e::PhoPredCollection>   ( reco::PhotonRefProd(hPhoton) );
-
   // Set association between photon ref (key) and products to be stored (val)
   for ( unsigned int iP = 0; iP < hPhoton->size(); iP++ ) {
     PhotonRef iRecoPho( hPhoton, iP );
     cPhoProbs->setValue( iP, vPhoProbs[iP] );
   } // photons
-
+    
+    
   // Put collections into output EDM file
   iEvent.put( std::move(cPhoProbs), "EGProbs" );
 
   return;
-
 } // EGTagger::produce()
 
 void
